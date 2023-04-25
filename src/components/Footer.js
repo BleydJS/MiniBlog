8a42ed07a31e23419258680b82aac9e76db0cc82
Bleydson.Mendonça <bleydsonanderson@hotmail.com>
2023-04-25 16:09:42 -0300

    adição de informativo responsividade

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <h3>Escreva sobre o que você tem interesse!</h3>
       <p>Riot Blog &copy; 2023</p>
+      <p style={{color: "red", fontWeight: "bold"}}>Responsividade para aparelhos móveis em andamento...</p>
     </footer>
   )
 }
